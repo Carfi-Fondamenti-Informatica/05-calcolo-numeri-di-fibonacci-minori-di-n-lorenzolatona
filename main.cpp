98a493e205af5d566b8a4d3e61a32b2a8e90dd85
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-11-25 16:28:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+   int i,n=0,x=1,y=1,z;
+    cin>>n;
+    cout<<x<<endl;
+    cout<<y<<endl;
+    for (int i=2;i<=n;i++){
+        z = x + y;
+        x = y;
+        y = z;
+        if (z<=n)
+            cout << z << endl;
+    }
    return 0;
 }
